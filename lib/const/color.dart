11f20f4778a26cd55ce288feb0baea6461e8cb37
satoruGojo007 <meerbalajkhaan007@gmail.com>
2024-10-22 15:45:44 +0500

    colors added

---
+++ lib/const/color.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF000000);
+  static const Color secondary = Color(0xFFFFFFFF);
+  static const Color lightGrey = Color(0xFFBCBCBC);
+  static const Color darkGrey = Color(0xFFFFFFF);
+  static const Color mediumGrey = Color(0xFFDDDDDD);
+  static const Color textFieldGrey = Color(0xFFE2E2E2);
+  static const Color darkBlue = Color(0xFF1F2937);
+  static const Color skyBlue = Color(0xFF35A4FF);
+  static const Color orange = Color(0xFFDB5C00);
+}
