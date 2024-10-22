92e275a4a4ee2499c2a3cd5a1755f0e464562859
satoruGojo007 <meerbalajkhaan007@gmail.com>
2024-10-22 18:05:29 +0500

    working on home screen

--- lib/const/svg_icons.dart
+++ lib/const/svg_icons.dart
@@ -3,6 +3,8 @@ class AppSvgs {
   static const String menu = "./assets/svgs/menu.svg";
   static const String back_button = "./assets/svgs/back_button.svg";
   static const String notification = "./assets/svgs/notification.svg";
+  static const String calender = "./assets/svgs/calender.svg";
+  static const String clock = "./assets/svgs/clock.svg";
 
 
   // Midat
--- lib/views/home_screen/Home_screen.dart
+++ lib/views/home_screen/Home_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:repairoo/const/color.dart';
 import 'package:repairoo/const/images.dart';
+import 'package:repairoo/const/svg_icons.dart';
 import 'package:repairoo/const/text_styles.dart';
 import 'package:repairoo/widgets/app_bars.dart';
+import 'package:repairoo/widgets/my_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -74,14 +76,104 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       SizedBox(height: 10.h,),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Container(
-
+                            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 14.h),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(14.w),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SizedBox(
+                                  child: Row(
+                                    children: [
+                                      MySvg(assetName: AppSvgs.calender),
+                                      SizedBox(width: 8.w,),
+                                      Text("Mon, Dec 23", style: montserrat600(11.sp, AppColors.primary),),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(width: 45.w,),
+                                SizedBox(
+                                  child: Row(
+                                    children: [
+                                      MySvg(assetName: AppSvgs.clock),
+                                      SizedBox(width: 8.w,),
+                                      Text("12:00", style: montserrat600(11.sp, AppColors.primary),),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 15.w,),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric( vertical: 14.h),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(14.w),
+                              ),
+                              alignment: Alignment.center,
+                              child: Text("Spare Parts", style: jost600(13.sp, AppColors.primary),),
+                            ),
                           )
+                        ],
+                      ),
+                      SizedBox(height: 10.h,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric( vertical: 14.h),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(14.w),
+                              ),
+                              alignment: Alignment.center,
+                              child: Text("View", style: jost600(13.sp, AppColors.primary),),
+                            ),
+                          ),
+                          SizedBox(width: 15.w,),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric( vertical: 14.h),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(14.w),
+                              ),
+                              alignment: Alignment.center,
+                              child: Text("Open Maps", style: jost600(13.sp, AppColors.primary),),
+                            ),
+                          ),
+                          SizedBox(width: 15.w,),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric( vertical: 14.h),
+                              decoration: BoxDecoration(
+                                color: AppColors.lightGrey,
+                                borderRadius: BorderRadius.circular(14.w),
+                              ),
+                              alignment: Alignment.center,
+                              child: Text("Reschedule", style: jost600(13.sp, AppColors.secondary),),
+                            ),
+                          ),
                         ],
                       )
                     ]
                   ),
+                ),
+                SizedBox(height: 18.h,),
+                Wrap(
+                  spacing: 0.8,
+                  alignment: WrapAlignment.center,
+                  children: [
+
+                  ],
                 )
             
               ],
