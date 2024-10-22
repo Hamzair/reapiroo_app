4081ef2d09481613dcb5c64118a8a873e643059a
Hamzair <152982499+Hamzair@users.noreply.github.com>
2024-10-22 15:15:44 +0500

    first commit..

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        builder: (_, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            // home: const SplashScreen(),
+            // initialBinding: UserBinding(),
+          );
+        });
+  }
+}
